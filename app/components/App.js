da3637f6f1d7566ebb7710561920ca31004b12f3
Aileen <asantos3026@gmail.com>
2017-05-07 17:42:39 -0700

    Complete functions to trigger when the API is called

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ var Popular = require('./Popular');
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 require('../index.css')
 
 // state
@@ -23,6 +24,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
